90f2e28c45ce79cfee7473dd7772fa997c777a0a
amanpanesar <aman.panesar5@gmail.com>
2026-05-15 11:36:19 +0100

    initial commit

--- CSharp/ProductExport/XmlExporterTest.cs
+++ CSharp/ProductExport/XmlExporterTest.cs
@@ -1,25 +1,30 @@
-using System;
 using System.Collections.Generic;
 using System.IO;
-using System.Text;
 using System.Threading.Tasks;
-using System.Xml.Linq;
-using VerifyTests;
+using Newtonsoft.Json;
 using VerifyXunit;
 using Xunit;
 
-namespace ProductExport
-{
-    [UsesVerify]
-    public class XmlExporterTest
-    {
-        [Fact]
-        public Task Something()
-        {
-            // TODO: finish writing this test
-            var xml = "";
-            return Verifier.VerifyXml(xml);
-        }
+namespace ProductExport;
 
+[UsesVerify]
+public class XmlExporterTest
+{
+    [Fact]
+    public Task Something()
+    {
+        var json = File.ReadAllText(Path.Combine(Directory.GetCurrentDirectory(),"Orders.json"));
+        var orders = JsonConvert.DeserializeObject<List<Order>>(json);
+
+        var xml = XmlExporter.ExportFull(orders);
+
+        return Verifier.VerifyXml(xml);
+    }
+    
+    [Fact]
+    public void Price()
+    {
+        var json = File.ReadAllText(Path.Combine(Directory.GetCurrentDirectory(),"Temp.json"));
+        var price = JsonConvert.DeserializeObject<Product>(json);
     }
 }
